b9df47319ff5fd5f4c1d255c6ab134a11ab496c7
pezzaliapp <pezzalialessandro@gmail.com>
2025-03-09 17:37:33 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,28 @@
+// service-worker.js
+const CACHE_NAME = "flexitirerent-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/icon/FlexiTireRent-192.png",
+  "/icon/FlexiTireRent-512.png"
+];
 
+// Installazione Service Worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("flexitirerent-cache-v1").then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Recupero dalla cache (offline support)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
